9359dd2d62b48bd2399f80a0ffc5489b3d666804
riquelminho <rachidriquelmi@gmail.com>
2023-04-24 15:31:38 -0700

    new update

--- _printf.c
+++ _printf.c
@@ -5,55 +5,55 @@
 
 
 /**
- * _printf - print output according to a format
- * @format: format string
- *
- * Return: number of characters printed
- */
+* _printf - print output according to a format
+* @format: format string
+*
+* Return: number of characters printed
+*/
 
 int _printf(const char *format, ...)
 {
-    va_list arg_list;
-    int count = 0;
+va_list arg_list;
+int count = 0;
 
-    va_start(arg_list, format);
+va_start(arg_list, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c')
-            {
-                int c = va_arg(arg_list, int);
-                putchar(c);
-                count++;
-            }
-            else if (*format == 's')
-            {
-                char *s = va_arg(arg_list, char *);
-                while (*s)
-                {
-                    putchar(*s);
-                    s++;
-                    count++;
-                }
-            }
-            else if (*format == '%')
-            {
-                putchar('%');
-                count++;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+int c = va_arg(arg_list, int);
+putchar(c);
+count++;
+}
+else if (*format == 's')
+{
+char *s = va_arg(arg_list, char *);
+while (*s)
+{
+putchar(*s);
+s++;
+count++;
+}
+}
+else if (*format == '%')
+{
+putchar('%');
+count++;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
 
-    va_end(arg_list);
+va_end(arg_list);
 
-    return (count);
+return (count);
 }
